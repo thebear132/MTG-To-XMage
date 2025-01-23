21f46c55a088d95305f5a8c4b25aa62b4cbd39c8
TheBear <drachmann.bjorn@gmail.com>
2025-01-23 20:43:30 +0100

    Moxfield-fix1

--- debugger.py
+++ debugger.py
@@ -1,11 +1,29 @@
+import browser_cookie3 as bc3
 import requests
+import re
+from time import sleep
+import json
 
-url = (
-            "https://api.moxfield.com/v2/users/" +
-            "thebear132" + "/decks?pageNumber=1&pageSize=99999"
-        )
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0'}
+
+cookiejar = bc3.firefox(domain_name="moxfield.com")
 proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
-r = requests.get(url, headers=headers, proxies=proxies, verify=False)
-print(r.text)
+useragent = "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0"
 
+resp = requests.get('https://moxfield.com', cookies=cookiejar, proxies=proxies, verify=False, headers={'User-Agent': useragent})
+print("Initial headers", resp.headers)
+
+sleep(1)
+
+burp0_url = "https://api2.moxfield.com:443/v1/account/token/refresh"
+burp0_headers = {"User-Agent": useragent, "Content-Type": "application/json"}
+burp0_json={"ignoreCookie": False, "isAppLogin": False}
+getAuth = requests.post(burp0_url, headers=burp0_headers, cookies=cookiejar, json=burp0_json, proxies=proxies, verify=False)
+print(getAuth)
+a = json.loads(getAuth.text)
+print(a)
+print(a["access_token"])
+
+
+resp = requests.get('https://api2.moxfield.com/v3/decks', cookies=cookiejar, proxies=proxies, verify=False, headers={'User-Agent': useragent, "Authorization": a["access_token"]})
+print(resp.status_code)
+print(resp.text)
